69e3a81a6a554430398b4fc435b82600d1b502c1
Deandre Gibson <DGibson@chicagotechacademy.org>
2014-01-31 14:58:47 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp2::Application.config.secret_token = '5b345ef61cdbc97cebd32d85aad09d7d38298f9e81d35cbf9105c28814ac9ff289922981be56dcdcf5c041e2bde9b239d67c3fe708dc6a3b269a704327df2d21'
